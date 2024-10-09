3ac6338fa8b726d193b1b2098871e3c8a207d835
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-09 12:38:08 +0200

    Add AWS instance resource and tags to main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
-profile = "default"
-region = "us-east-1"
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "clava_instance" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+}
+
+tags = {
+    Name = "clava_instance"
 }
